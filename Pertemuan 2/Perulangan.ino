90974bdbcf5ccd55ce0e3d05fcac8503a896569e
Fahriza Sallam <fahrizasallam@gmail.com>
2026-04-07 21:10:39 +0700

    Add files via upload

---
+++ Pertemuan 2/Perulangan.ino
@@ -0,0 +1,36 @@
+int timer = 100; // delay. Semakin tinggi angkanya,semakin lambat waktunya.
+
+void setup() {
+  // gunakan loop for untuk menginisialisasi setiap pin sebagai
+  output:
+  for (int ledPin = 2; ledPin < 8; ledPin++) {
+    pinMode(ledPin, OUTPUT);
+  }
+}
+
+void loop() {
+  // looping dari pin rendah ke tinggi
+  for (int ledPin = 2; ledPin < 8; ledPin++) {
+    // hidupkan LED pin nya:
+    digitalWrite(ledPin, HIGH);
+    delay(timer);
+    // matikan pin LED nya:
+    digitalWrite(ledPin, LOW);
+  }
+  // looping dari pin yang tinggi ke yang rendah
+  for (int ledPin = 7; ledPin >= 2; ledPin--) {
+    // menghidupkan pin:
+    digitalWrite(ledPin, HIGH);
+    delay(timer);
+    // mematikan pin:
+    digitalWrite(ledPin, LOW);
+  }
+}void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
